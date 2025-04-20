ea52cd536dfef63c4eb144120b563d3de502df35
Souvik Roy <dev.souvikroy92@gmail.com>
2025-04-20 23:30:19 +0530

    chore: adding vercel config for deployment after some changes

--- index.js
+++ index.js
@@ -58,7 +58,9 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
